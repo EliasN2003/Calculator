7e1a1e5c1196201153b7770ea5b773d1038dffd3
eliasn2003 <110858094+EliasN2003@users.noreply.github.com>
2022-08-16 03:12:56 +0300

    Small fixes and comments
    Swapping some integers for floats,
    adding comments,

--- Calculator.py
+++ Calculator.py
@@ -1,71 +1,83 @@
 from tkinter import *
 
+# GUI Calculator
+
 root = Tk()
 root.title("Simple calculator")
 
+# The entry bar
 Entry_bar = Entry(root, width=35, borderwidth=5)
 Entry_bar.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 
+# Function for a number clicked
 def button_click(number):
-    current = Entry_bar.get()
-    Entry_bar.delete(0, END)
-    Entry_bar.insert(0, str(current) + str(number))
+    current = Entry_bar.get()       # Saves the typed number
+    Entry_bar.delete(0, END)        # Clears entry bar
+    Entry_bar.insert(0, str(current) + str(number))   # Prints the saved number and then the new number on the entry bar
 
 
+# Clear button
 def button_clear():
-    Entry_bar.delete(0, END)
+    Entry_bar.delete(0, END)        # Clears the entry bar
 
 
+# Add button
 def button_plus():
     global calculation
     calculation = "Addition"
     global f_num
-    f_num = int(Entry_bar.get())
+    f_num = float(Entry_bar.get())
     Entry_bar.delete(0, END)
 
 
+# Equals button
 def button_equal():
-    sec_num = int(Entry_bar.get())
+    sec_num = float(Entry_bar.get())      # Saves the second number typed
     Entry_bar.delete(0, END)
 
+    # Checks what calculation we are doing
     if calculation == "Addition":
-        Entry_bar.insert(0, f_num + sec_num)
+        Entry_bar.insert(0, str(f_num + sec_num))
 
     elif calculation == "Multiplication":
-        Entry_bar.insert(0, f_num * sec_num)
+        Entry_bar.insert(0, str(f_num * sec_num))
 
     elif calculation == "Division":
-        Entry_bar.insert(0, f_num / sec_num)
+        Entry_bar.insert(0, str(f_num / sec_num))
 
     elif calculation == "Subtraction":
-        Entry_bar.insert(0, f_num - sec_num)
+        Entry_bar.insert(0, str(f_num - sec_num))
 
 
+# Multiplication button
 def button_mul():
     global calculation
     calculation = "Multiplication"
     global f_num
-    f_num = int(Entry_bar.get())
+    f_num = float(Entry_bar.get())
     Entry_bar.delete(0, END)
 
 
+# Division button
 def button_div():
     global calculation
     calculation = "Division"
     global f_num
-    f_num = int(Entry_bar.get())
+    f_num = float(Entry_bar.get())
     Entry_bar.delete(0, END)
 
 
+# Subtraction button
 def button_sub():
     global calculation
     calculation = "Subtraction"
     global f_num
-    f_num = int(Entry_bar.get())
+    f_num = float(Entry_bar.get())
     Entry_bar.delete(0, END)
 
 
+# Creation of buttons
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
 button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
@@ -84,6 +96,7 @@ button_div = Button(root, text="/", padx=41, pady=20, command=button_div)
 button_sub = Button(root, text="-", padx=41, pady=20, command=button_sub)
 
 
+# Placement of buttons
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
 button_3.grid(row=3, column=2)
